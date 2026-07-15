12de293d1372070e75c80ea4ee1779b7ec9714f5
Tristan Cravello <tlcravello@gmail.com>
2026-07-15 12:46:29 -0400

    Fix CI tests for deploy profile

--- src/test/java/com/threatpipeline/threatingest/ThreatIngestApplicationTests.java
+++ src/test/java/com/threatpipeline/threatingest/ThreatIngestApplicationTests.java
@@ -2,8 +2,10 @@ package com.threatpipeline.threatingest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("deploy")
 class ThreatIngestApplicationTests {
 
 	@Test
